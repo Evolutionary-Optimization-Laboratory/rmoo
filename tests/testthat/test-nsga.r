cb525c84a624b966720753b96dd7672eca78dd51
Francisco_Benitez <benitez.fj@hotmail.com>
2020-10-17 15:42:49 -0400

    - Test for nsga and nsga2 added.-

---
+++ tests/testthat/test-nsga.r
@@ -0,0 +1,21 @@
+context("Executing nsga")
+
+test_that("nsga() return the value suggestions", {
+  testfunction <- function (x){
+    return(x)
+  }
+
+  x <- matrix(rep(1,30), ncol = 3)
+
+  out <- nsga(type = "real-valued", fitness = testfunction,
+              lower = c(0,0,0), upper = c(1,1,1),
+              popSize = 10, suggestions = x,
+              pcrossover = 0, pmutation = 0,
+              seed = 1, dshare = 1, monitor = FALSE,
+              maxiter = 1, nObj = 3)
+
+  expect_identical(out@fitness, x)
+})
+
+
+
---
+++ tests/testthat/test-nsga2.r
@@ -0,0 +1,21 @@
+context("Executing nsga2")
+
+test_that("nsga2() return the value suggestions", {
+  testfunction <- function (x){
+    return(x)
+  }
+
+  x <- matrix(rep(1,30), ncol = 3)
+
+  out <- nsga2(type = "real-valued", fitness = testfunction,
+               lower = c(0,0,0), upper = c(1,1,1), popSize = 10,
+               suggestions = x, pcrossover = 0, pmutation = 0,
+               seed = 1, monitor = FALSE, maxiter = 1, nObj = 3)
+
+  expect_identical(out@fitness, x)
+})
+
+
+
+
+
